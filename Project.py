ffb69df3e1f55e52a20fea1521c6991d318d050b
Trần Hồng Quân <hongquan95.tran@gmail.com>
2017-12-11 08:34:30 +0700

    Eo nghich nua

--- Project.py
+++ Project.py
@@ -8,18 +8,20 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 
 t1 =time()
-#Doc Data_train
+#1.Read Train Data from CSV
 read_data = pd.read_csv('/media/top/TOP G/PROJECT_2/IrisProject/file/Train_data.csv')
 data = read_data.values
+np.random.shuffle(data) ########################3
 
 Y_train = data[:,1]
 X_train_pca = np.delete(data,[0,1],1)
 
-#Doc Data_test
+#Read Test Data from CSV
 read_data2 = pd.read_csv('/media/top/TOP G/PROJECT_2/IrisProject/file/Test_data.csv')
 data2 = read_data2.values
+np.random.shuffle(data2)#######################
 
-Y_test = data[:,1]
+Y_test = data2[:,1]
 X_test_pca = np.delete(data2,[0,1],1)
 
 print("time chuan bi data: %0.3f"%(time()-t1))
@@ -28,26 +30,27 @@ def predict(model, X):
 	return model.predict(X)
 
 
-num_neuron = np.array([20,25])
+#3. Build Model
+num_neuron = np.array([20])
 
 t2 = time()
 for i in (num_neuron):
-	mlp = MLPClassifier(hidden_layer_sizes=(i,),max_iter=400,activation='relu',solver='sgd',
-	                        learning_rate_init=0.001,tol=1e-4,random_state=1,verbose=False)
+	mlp = MLPClassifier(hidden_layer_sizes=(i,),max_iter=500,activation='relu',solver='sgd',
+	                        learning_rate_init=0.001,tol=1e-4,random_state=1,verbose=True)
 	mlp.fit(X_train_pca,Y_train)
 
 	Y_predict = predict(mlp,X_test_pca)
 	print("Loss of %d Neuron of Hidden Layer: %0.6f"%(i,mlp.loss_))
-	print("Score of %d Neuron of Hidden Layer: %d"%(i,accuracy_score(Y_test, Y_predict, normalize=False)))
+	print("Score of %d Neuron of Hidden Layer: %d/%d"%(i,accuracy_score(Y_test, Y_predict, normalize=False),len(Y_test)))
 	#from sklearn.metrics import classification_report
 	from sklearn.metrics import precision_recall_fscore_support
 	print("Neuron %d"%i)
 	print(precision_recall_fscore_support(Y_test, Y_predict, average = 'weighted'))
 
-# 65 la ra loss tot nhat
+
 print("Time train NN = %0.3fs"%(time()-t2))
 
-
+#
 
 
 
--- chuanbidulieu.py
+++ chuanbidulieu.py
@@ -16,14 +16,14 @@ np.seterr(divide='ignore', invalid='ignore')
 
 def build_list(path,vx,vy):
 	list_fn = []
-	for i in range(vx):
+	for i in range(1,vx+1):
 		for j in (vy):
-			fn = path+str(i).zfill(3) + '/' +'S6'+str(i).zfill(3) +'S'+ str(j).zfill(2) + '.jpg'
+			fn = path+str(i).zfill(1) + '_' +str(j).zfill(1)+'.jpg'
 			list_fn.append(fn)
 	return list_fn
 
 def build_data(list_name, x, y, r,h):
-	raito = 0.1
+	raito = 1  #CHINH RESIZE
 	a = int(r*raito)
 	b = int(h*raito)
 	X_full = np.zeros((x*y,a*b))
@@ -47,15 +47,15 @@ def lebel(nx,ny):
 	return Y
 
 
-#Train data
+#1.Train data
 
-path_train='/media/top/TOP G/database1/IRIS/CASIA-Iris-Syn/'
+path_train='/media/top/TOP G/database1/IRIS/PHONG/'
 # list_tr = [".centerlight",".glasses",".happy",".leftlight",".normal",".rightlight",".sad",".sleepy",".surprised",".wink"]
-list_tr = np.arange(5)
-n_person = 500
+list_tr = np.array([1,2,3,4,7]);
+n_person = 49
 n_pic = 5
-x = 480
-y = 640
+x = 60
+y = 440
 list_train = build_list(path_train,n_person,list_tr)
 X_train = build_data(list_train,n_person, n_pic,x,y)
 
@@ -64,7 +64,7 @@ scaler.fit(X_train)
 X_train_norm = scaler.transform(X_train)
 
 t1 = time()
-pca = PCA(n_components =100,svd_solver='full' ).fit(X_train_norm)
+pca = PCA(n_components =170,svd_solver='full' ).fit(X_train_norm)
 X_train_pca = pca.transform(X_train_norm)
 
 
@@ -72,26 +72,26 @@ print("Time PCA = %0.3fs"%(time()-t1))
 
 Y_train = lebel(n_person,n_pic)
 
-
+#2.Save Train Data to CSV
 data =  np.concatenate((Y_train,X_train_pca),axis=1)
 df = pd.DataFrame(data)
 df.to_csv('/media/top/TOP G/PROJECT_2/IrisProject/file/Train_data.csv')
 
 
 
-#Test data
+#3.Test data
 
-path_test='/media/top/TOP G/database1/IRIS/CASIA-Iris-Syn/'
-list_te = np.array([5,6,7,8,9])
-n_pic_test = 5
+path_test='/media/top/TOP G/database1/IRIS/PHONG/'
+list_te = np.array([5,6])
+n_pic_test = 2
 list_test = build_list(path_test,n_person,list_te)
 X_test = build_data(list_test,n_person, n_pic_test,x,y)
 
 X_test_norm = scaler.transform(X_test)
 X_test_pca = pca.transform(X_test_norm)
-
 Y_test = lebel(n_person,n_pic_test)
 
+#4.Save Test Data to CSV
 data2 =  np.concatenate((Y_test,X_test_pca),axis=1)
 df2 = pd.DataFrame(data2)
 df2.to_csv('/media/top/TOP G/PROJECT_2/IrisProject/file/Test_data.csv')
